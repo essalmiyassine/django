57a29957721f13872f1b5e90d4ed79b7dda51524
CorrectUsername <CorrectEmail@example.com>
2025-02-03 16:51:02 +0100

    rename root file to configurations

--- configurations/settings.py
+++ configurations/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_socle.urls'
+ROOT_URLCONF = 'configurations.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_socle.wsgi.application'
+WSGI_APPLICATION = 'configurations.wsgi.application'
 
 
 # Database
